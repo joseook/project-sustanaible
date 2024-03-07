65018a777502da87280ca7327371695f7ef35e0e
José <pauloowneto@gmail.com>
2024-03-07 00:12:11 -0300

    Update config added responsives and tags colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     container: {
@@ -9,13 +9,32 @@ export default {
       padding: "2rem",
     },
     screens: {
+      xs: '400px',
       sm: '640px',
       md: '768px',
       lg: '1024px',
       xl: '1280px',
     },
-
+    colors: {
+      primary: '#4C51BF', 
+      secondary: '#10B981', 
+    },
+    textColor: {
+      primary: '#4C51BF',
+      secondary: '#10B981',
+      white: '#FFFFFF',
+    },
+    backgroundColor: {
+      primary: '#4C51BF', 
+      secondary: '#10B981', 
+      black: '#0000',
+      white: '#FFFFFF',
+      gray: '#F5F5F5',
+      lightGray: '#F5F5F5',
+      darkGray: '#4C51BF',
+      blue: '#10B981',
+    
+    },
+  },
   plugins: [],
-  }
 }
-
